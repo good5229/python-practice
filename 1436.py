3a05baa2476b1f1cdb18e6fa16fe2dc505338761
김종백 <kjb45841497@gmail.com>
2020-02-02 20:23:31 +0900

    feat:solve 1436

--- 1436.py
+++ 1436.py
@@ -1,24 +1,17 @@
 import sys
 
 
-# def movie():
-#     n = int(sys.stdin.readline().strip())
-#     movie = 666
-#     while n:
-#         if "666" in str(movie):
-#             n -= 1
-#             movie += 1
-#     print(movie - 1)
-#
-#
-# print(movie())
-
-def star():
+def movie():
     n = int(sys.stdin.readline().strip())
-    for i in range(1, n + 1):
-        print(' ' * (i - 1), end='')
-        print('*' * (n - i + 1), end='')
-        print('')
+    count = 0
+    v = 0
+    while True:
+        if '666' in str(v):
+            count += 1
+        if count == n:
+            break
+        v += 1
+    return v
 
+print(movie())
 
-star()
