db08242b505415095c021eb220d199ded0454dbd
김종백 <kjb45841497@gmail.com>
2020-02-05 12:42:09 +0900

    feat:solve 11866
    Similar with 1158

---
+++ 11866.py
@@ -0,0 +1,13 @@
+import sys
+
+n, k = map(int, sys.stdin.readline().split())
+arr = list(range(1, n + 1))
+ans = []
+i = k - 1
+while True:
+    ans.append(arr.pop(i))
+    if not arr:
+        break
+    i = (i + k - 1) % len(arr)
+
+print('<' + ', '.join(map(str, ans)) + '>')
