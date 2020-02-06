d8ab45e3971a40c3d24300d1cbea93d61bf2780c
김종백 <kjb45841497@gmail.com>
2020-02-06 22:06:13 +0900

    feat:solve 15654

--- 15654.py
+++ 15654.py
@@ -4,7 +4,7 @@ import sys
 n, k = map(int, sys.stdin.readline().split())
 num_list = list(map(int, sys.stdin.readline().split()))
 num_list.sort()
-for s in itertools.permutations(num_list, k):
+for s in itertools.combinations(num_list, k):
     for j in s:
         print(j, end=' ')
     print('')
