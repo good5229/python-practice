0afb58f987ab4b6172e8558fc3d3cbcb5aa37122
김종백 <kjb45841497@gmail.com>
2020-02-06 17:01:20 +0900

    feat: solve 15654

---
+++ 15654.py
@@ -0,0 +1,10 @@
+import itertools
+import sys
+
+n, k = map(int, sys.stdin.readline().split())
+num_list = list(map(int, sys.stdin.readline().split()))
+num_list.sort()
+for s in itertools.permutations(num_list, k):
+    for j in s:
+        print(j, end=' ')
+    print('')
